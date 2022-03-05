2f1285f2a0f9595c31727b89490ba300edda31cd
sumitpehu <41047114+sumitpehu@users.noreply.github.com>
2022-03-05 16:27:33 +0530

    Add files via upload

---
+++ prefix sum.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // your code goes here
+    int arr[100]={0};
+    int n;
+    cin>>n;
+    int largests=0;
+    int cur;
+    for(int i =0; i<n; i++)
+    {
+    	cin>>arr[i];
+    }
+    int sum[n]={0};
+    sum[0]=arr[0];
+    for(int i =1; i<n; i++)
+    {
+    	sum[i]=arr[i]+sum[i-1];
+    }
+
+    for(int i=0; i<n; i++)
+    {
+    	for(int j=i; j<n; j++)
+    	{
+    		if(i>0)
+    		{
+              cur=sum[j]- sum[i-1];
+    		}
+    		else{
+    			cur=sum[j];
+    		}
+    		largests=max(largests,cur);
+    	}
+    }
+    cout<<largests<<endl;
+    
+     
+}
+
